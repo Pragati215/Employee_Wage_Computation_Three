8c57d2a592374c1652a2621c75efb1228b530383
Pragati215 <dhakanepragati4@gmail.com>
2020-11-30 23:55:14 +0530

    Modified:total Employee Wage for months

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -5,7 +5,9 @@ console.log('welcome to Employee Wage Computation')
          const FULL_TIME_HOURS = 8;
          const WAGE_PER_HOUR = 20;
          const NUM_OF_WORKING_DAYS = 20;
-
+         const MAX_HRS_IN_MONTH = 100;
+         let totalEmpHrs = 0;
+         let totalWorkingDays = 0;
          function getWorkingHours(empCheck) {
          switch (empCheck) {
                  case IS_FULL_TIME:
@@ -20,10 +22,12 @@ console.log('welcome to Employee Wage Computation')
             }
               let empHrs = 0;
               empCheck = Math.floor(Math.random() * 10) % 3;
-              for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+              while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+                totalWorkingDays++;
                 let empCheck = Math.floor(Math.random() * 10) % 3;
-                   empHrs += getWorkingHours(empCheck);
-                }
-                 let empWage = empHrs * WAGE_PER_HOUR;
-                 console.log("Total Hrs: " +empHrs+ " emp Wage: " + empWage);
-              
+                totalEmpHrs +=  getWorkingHours(empCheck);
+             }
+                let empWage = totalEmpHrs * WAGE_PER_HOUR;
+               console.log("Total Days: " + totalWorkingDays + "Total Hrs: " +totalEmpHrs+ " Emp Wage: " + empWage);
+         
+                    
