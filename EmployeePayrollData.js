4e576eca0f7c882893a7e1edc0489a81b4661d5f
Pragati215 <dhakanepragati4@gmail.com>
2020-12-18 22:44:43 +0530

    Refactor: Regex is added to validate employee payroll data

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -1,11 +1,4 @@
 class EmployeePayrollData {
-    // property
-    id;
-    salary;
-    gender;
-    startDate;
-
-    //constructor
     constructor(...params) {
         this.id = params[0];
         this.name = params[1];
@@ -13,37 +6,80 @@ class EmployeePayrollData {
         this.gender = params[3];
         this.startDate = params[4];
     }
-
-    //getter and setter method
-    get name() { return this._name; }
-    set name(name) { this._name = name; 
-        let nameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
-        if (nameRegex.test(name))
-         this._name = name;
-        else throw 'name is Incorrect!';     
+    get name() {
+        return this._name;
+    }
+    set name(name) {
+        let nRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+        if (nRegex.test(name)) {
+            this._name = name;
+        } else throw "Invalid Name";
     }
 
+    get id() {
+        return this._id;
+    }
+    set id(id) {
+        let idRegex = RegExp("^[1-9]{1}[0-9]*$");
+        if (idRegex.test(id)) {
+            this._id = id;
+        } else throw "Invalid id";
+    }
 
-    //method
+    get salary() {
+        return this._salary;
+    }
+    set salary(salary) {
+        let sRegex = RegExp("^[1-9]{1}[0-9]*$");
+        if (sRegex.test(salary)) {
+            this._salary = salary;
+        } else throw "Invalid salary";
+    }
+    get gender() {
+        return this._gender;
+    }
+    set gender(gender) {
+        let gRegex = RegExp("^[MF]$");
+        if (gRegex.test(gender)) {
+            this._gender = gender;
+        } else throw "Invalid gender";
+    }
+
+    get startDate() {
+        return this._startDate;
+    }
+    set startDate(startDate) {
+        let date = new Date();
+        if (startDate < date) {
+            this._startDate = startDate;
+        } else throw "Invalid Date";
+    }
+
     toString() {
-        const options = { year: 'numeric', month: 'long', day: 'numeric' };
-        const empDate = this.startDate === undefined ? "undefined" : this.startDate.toLocaleDateString("en-US", options);
-        return "id= " + this.id + ", name ='" + this.name + ", salary = " + this.salary + ", " + "gender = " + this.gender + ", startDate = " + empDate;;
-
+        const options = { year: "numeric", month: "long", day: "numeric" };
+        const empDate =
+            this.startDate === undefined
+                ? "undefined"
+                : this.startDate.toLocaleDateString("en-US", options);
+        return (
+            "id: " + this.id +
+            ", name=" + this.name +
+            ", salary=" + this.salary +
+            ", gender: " + this.gender +
+            ", startDate: " + empDate
+        );
     }
 }
-let employeePayrollData = new EmployeePayrollData(1, "Mark", 30000);
-console.log(employeePayrollData.toString());
+
 try {
-    employeePayrollData.name = "john";
+    let employeePayrollData = new EmployeePayrollData(
+        1, "John", 30000, "F", new Date());
     console.log(employeePayrollData.toString());
-    } catch (e) {
-          console.error(e);
-    }    
-let newEmployeePayrollData = new EmployeePayrollData(1, "Terrisa", 30000, "F" , new Date());
-console.log(newEmployeePayrollData.toString());
 
-
-
-
+    let empPayrollData = new EmployeePayrollData(
+        2, "Rony", 00000, "M", new Date());
+    console.log(empPayrollData.toString());
+} catch (e) {
+    console.error(e);
+}
 
